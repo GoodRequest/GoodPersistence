780e03a1e21948b114d7af3bc0cfe0d8aecae4c0
Andrej Jasso <andrej.jasso@goodrequest.com>
2023-02-14 20:18:59 +0100

    build: Fixed compilation errors

--- Package.swift
+++ Package.swift
@@ -23,7 +23,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "GoodPersistence",
-            dependencies: ["CombineExt"],
+            dependencies: [
+                .product(name: "CombineExt", package: "CombineExt")
+            ],
             path: "./Sources/GoodPersistence"
         ),
         .testTarget(
